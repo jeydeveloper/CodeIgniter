5b537d93616604c5ae1c933fcb73cc887ac2723e
andysociolla <andy.juliadi@sociolla.com>
2019-10-27 16:36:28 +0700

    add assets

---
+++ application/views/backend/content.php
@@ -0,0 +1,119 @@
+<body>
+    <div id="sb-site">
+	    <div id="loading">
+	        <div class="spinner">
+	            <div class="bounce1"></div>
+	            <div class="bounce2"></div>
+	            <div class="bounce3"></div>
+	        </div>
+	    </div>
+	    <div id="page-wrapper">
+	    	<div id="page-header" class="bg-gradient-9">
+	    		<div id="mobile-navigation">
+			        <button id="nav-toggle" class="collapsed" data-toggle="collapse" data-target="#page-sidebar"><span></span></button>
+			        <a href="index.html" class="logo-content-small" title="MonarchUI"></a>
+			    </div>
+			    <div id="header-logo" class="logo-bg">
+			        <a href="index.html" class="logo-content-big" title="MonarchUI">
+			            Monarch <i>UI</i>
+			            <span>The perfect solution for user interfaces</span>
+			        </a>
+			        <a href="index.html" class="logo-content-small" title="MonarchUI">
+			            Monarch <i>UI</i>
+			            <span>The perfect solution for user interfaces</span>
+			        </a>
+			        <a id="close-sidebar" href="#" title="Close sidebar">
+			            <i class="glyph-icon icon-angle-left"></i>
+			        </a>
+			    </div>
+			    <div id="header-nav-left">
+			        <div class="user-account-btn dropdown">
+			            <a href="#" title="My Account" class="user-profile clearfix" data-toggle="dropdown">
+			                <img width="28" src="../../assets/image-resources/gravatar.jpg" alt="Profile image">
+			                <span>Thomas Barnes</span>
+			                <i class="glyph-icon icon-angle-down"></i>
+			            </a>
+			            <div class="dropdown-menu float-left">
+			                <div class="box-sm">
+			                    <div class="login-box clearfix">
+			                        <div class="user-img">
+			                            <a href="#" title="" class="change-img">Change photo</a>
+			                            <img src="../../assets/image-resources/gravatar.jpg" alt="">
+			                        </div>
+			                        <div class="user-info">
+			                            <span>
+			                                Thomas Barnes
+			                                <i>UX/UI developer</i>
+			                            </span>
+			                            <a href="#" title="Edit profile">Edit profile</a>
+			                            <a href="#" title="View notifications">View notifications</a>
+			                        </div>
+			                    </div>
+			                    <div class="divider"></div>
+			                    <ul class="reset-ul mrg5B">
+			                        <li>
+			                            <a href="#">
+			                                View login page example
+			                                <i class="glyph-icon float-right icon-caret-right"></i>
+			                            </a>
+			                        </li>
+			                        <li>
+			                            <a href="#">
+			                                View lockscreen example
+			                                <i class="glyph-icon float-right icon-caret-right"></i>
+			                            </a>
+			                        </li>
+			                        <li>
+			                            <a href="#">
+			                                View account details
+			                                <i class="glyph-icon float-right icon-caret-right"></i>
+			                            </a>
+			                        </li>
+			                    </ul>
+			                    <div class="pad5A button-pane button-pane-alt text-center">
+			                        <a href="#" class="btn display-block font-normal btn-danger">
+			                            <i class="glyph-icon icon-power-off"></i>
+			                            Logout
+			                        </a>
+			                    </div>
+			                </div>
+			            </div>
+			        </div>
+			    </div><!-- #header-nav-left -->
+			    <div id="header-nav-right">
+			        <a href="#" class="hdr-btn" id="fullscreen-btn" title="Fullscreen">
+			            <i class="glyph-icon icon-arrows-alt"></i>
+			        </a>
+			    </div><!-- #header-nav-right -->
+			</div>
+			<div id="page-sidebar">
+				<div class="scroll-sidebar">
+					<ul id="sidebar-menu">
+						<li class="header"><span>Overview</span></li>
+					    <li>
+					        <a href="index.html" title="Admin Dashboard">
+					            <i class="glyph-icon icon-linecons-tv"></i>
+					            <span>Admin dashboard</span>
+					        </a>
+					    </li>
+					</ul>
+				</div>
+			</div>
+			<div id="page-content-wrapper">
+				<div id="page-content">
+					<div id="page-title">
+						<h2>Responsive data tables</h2>
+    					<p>Tables with a lot of advanced, easy to use features and options.</p>
+					</div>
+					<div class="panel">
+						<div class="panel-body">
+							<h3 class="title-hero">
+							    Responsive data tables
+							</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
