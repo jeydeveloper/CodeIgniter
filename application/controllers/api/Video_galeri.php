e8542c33d7a9cfba06e3d2b908cdc00634024823
andysociolla <andy.juliadi@sociolla.com>
2019-11-17 02:07:09 +0700

    update

--- application/controllers/api/Video_galeri.php
+++ application/controllers/api/Video_galeri.php
@@ -10,60 +10,108 @@ class Video_galeri extends REST_Controller {
     function __construct($config = 'rest') {
         parent::__construct($config);
         $this->load->database();
+
+        $this->load->model('videogaleri_model');
     }
 
-    function index_get() { //get data video_galeri
-        $id = $this->get('id_video_galeri');
-        if ($id == '') {
-            $kontak = $this->db->get('video_galeri')->result();
+    function index_get() { //get data menu
+        $id = $this->get('id');
+        $search = $this->get('search');
+        $columns = $this->get('columns');
+        $start = !empty($this->get('start')) ? $this->get('start') : 0;
+        $length = !empty($this->get('length')) ? $this->get('length') : 10;
+        $arr = [
+        	'no',
+        	'nama_video_galeri',
+        	'url_video_galeri',
+        	'keterangan_video_galeri',
+        	'action',
+        ];
+        if (!empty($id)) {
+        	$this->videogaleri_model->where('id_video_galeri = "'.$id.'"');
+            $videogaleri = $this->videogaleri_model->get_row();
+        } elseif (!empty($search['value'])) {
+        	$all = $this->videogaleri_model->get_all();
+        	foreach ($columns as $key => $value) {
+        		if($value['searchable'] == 'true') {
+        			$this->videogaleri_model->or_like($arr[$key], $search['value']);
+        		}
+        	}
+        	$filtered = $this->videogaleri_model->get_all();
+        	foreach ($columns as $key => $value) {
+        		if($value['searchable'] == 'true') {
+        			$this->videogaleri_model->or_like($arr[$key], $search['value']);
+        		}
+        	}
+        	$this->videogaleri_model->set_limit($length, $start);
+            $videogaleri = $this->videogaleri_model->get_all();
+            $videogaleri = $this->getRowDatatable($videogaleri);
+            $videogaleri = formatDatatable($videogaleri, $all, $filtered);
         } else {
-            $this->db->where('id_video_galeri', $id);
-            $kontak = $this->db->get('video_galeri')->result();
+            $all = $this->videogaleri_model->get_all();
+        	$this->videogaleri_model->set_limit($length, $start);
+            $videogaleri = $this->videogaleri_model->get_all();
+            $videogaleri = $this->getRowDatatable($videogaleri);
+            $videogaleri = formatDatatable($videogaleri, $all, $all);
         }
-        $this->response($kontak, 200);
+        $this->response($videogaleri, 200);
     }
 
-    function index_post() { // post data video_galeri
+    function index_post() { // post data menu
        	$data = array(
-		   'id_video_galeri' => $this->post('id_video_galeri'),
 		   'nama_video_galeri' => $this->post('nama_video_galeri'),
 		   'url_video_galeri' => $this->post('url_video_galeri'),
 		   'keterangan_video_galeri' => $this->post('keterangan_video_galeri'),
 		);
-		$insert = $this->db->insert('video_galeri', $data);
+		$insert = $this->videogaleri_model->insert($data);
 		if ($insert) {
-		   $this->response($data, 200);
+		   $this->response(array('status' => 'success'), 200);
 		} else {
 		   $this->response(array('status' => 'fail', 502));
 		}
    	}
 
-   	function index_put() { // update data kontak
-		$id = $this->put('id_video_galeri');
+   	function index_put() { // update data menu
+		$id = $this->put('id');
 		$data = array(
-			'id_video_galeri' => $this->put('id_video_galeri'),
 			'nama_video_galeri' => $this->put('nama_video_galeri'),
 		   	'url_video_galeri' => $this->put('url_video_galeri'),
 		   	'keterangan_video_galeri' => $this->put('keterangan_video_galeri'),
 		);
-		$this->db->where('id_video_galeri', $id);
-		$update = $this->db->update('video_galeri', $data);
+		$this->videogaleri_model->where('id_video_galeri = "'.$id.'"');
+		$update = $this->videogaleri_model->update($data);
 		if ($update) {
-			$this->response($data, 200);
+			$this->response(array('status' => 'success'), 200);
 		} else {
 			$this->response(array('status' => 'fail', 502));
 		}
  	}
 
-	function index_delete() { // delete data video_galeri
-		$id = $this->delete('id_video_galeri');
-		$this->db->where('id_video_galeri', $id);
-		$delete = $this->db->delete('video_galeri');
+	function index_delete() { // delete data menu
+		$id = $this->delete('id');
+		$this->videogaleri_model->where('id_video_galeri = "'.$id.'"');
+		$delete = $this->videogaleri_model->delete();
 		if ($delete) {
 		    $this->response(array('status' => 'success'), 201);
 		} else {
 		    $this->response(array('status' => 'fail', 502));
 		}
+	}
+
+	private function getRowDatatable($data = null) {
+		$ret = [];
+		$start = $this->get('start');
+		foreach ($data as $key => $value) {
+			$start += 1;
+			$ret[$key] = [
+				$start,
+				$value->nama_video_galeri,
+				$value->url_video_galeri,
+				$value->keterangan_video_galeri,
+				'<a onclick="doFormEdit('.$value->id_video_galeri.');return false;" href="#" class="btn btn-xs btn-success">EDIT <i class="glyph-icon icon-pencil-square-o"></i></a> <a onclick="showModalBoxDelete('.$value->id_video_galeri.');return false;" href="#" class="btn btn-xs btn-danger">DELETE <i class="glyph-icon icon-close"></i></a>',
+			];
+		}
+		return $ret;
 	}
 
 }
