7006c948dbeaa4ebed66eeb04c2f86011cffd19b
andysociolla <andy.juliadi@sociolla.com>
2019-11-16 21:26:13 +0700

    update

--- application/controllers/api/Submenu.php
+++ application/controllers/api/Submenu.php
@@ -10,22 +10,52 @@ class Submenu extends REST_Controller {
     function __construct($config = 'rest') {
         parent::__construct($config);
         $this->load->database();
+
+        $this->load->model('submenu_model');
     }
 
-    function index_get() { //get data sub_menu
-        $id = $this->get('id_sub_menu');
-        if ($id == '') {
-            $kontak = $this->db->get('sub_menu')->result();
+    function index_get() { //get data menu
+        $id = $this->get('id');
+        $search = $this->get('search');
+        $columns = $this->get('columns');
+        $start = !empty($this->get('start')) ? $this->get('start') : 0;
+        $length = !empty($this->get('length')) ? $this->get('length') : 10;
+        $arr = [
+        	'no',
+        	'nama_sub_menu',
+        	'id_menu',
+        	'style_sub_menu',
+        	'position_sub_menu',
+        	'grup_sub_menu',
+        	'link',
+        	'action',
+        ];
+        if (!empty($id)) {
+        	$this->submenu_model->where('id_sub_menu = "'.$id.'"');
+            $menu = $this->submenu_model->get_row();
+        } elseif (!empty($search['value'])) {
+        	$all = $this->submenu_model->get_all();
+        	foreach ($columns as $key => $value) {
+        		if($value['searchable'] == 'true') {
+        			$this->submenu_model->or_like($arr[$key], $search['value']);
+        		}
+        	}
+        	$this->submenu_model->set_limit($length, $start);
+            $menu = $this->submenu_model->get_all();
+            $menu = $this->getRowDatatable($menu);
+            $menu = formatDatatable($menu, $all);
         } else {
-            $this->db->where('id_sub_menu', $id);
-            $kontak = $this->db->get('sub_menu')->result();
+            $all = $this->submenu_model->get_all();
+        	$this->submenu_model->set_limit($length, $start);
+            $menu = $this->submenu_model->get_all();
+            $menu = $this->getRowDatatable($menu);
+            $menu = formatDatatable($menu, $all);
         }
-        $this->response($kontak, 200);
+        $this->response($menu, 200);
     }
 
-    function index_post() { // post data sub_menu
+    function index_post() { // post data menu
        	$data = array(
-		   'id_sub_menu' => $this->post('id_sub_menu'),
 		   'nama_sub_menu' => $this->post('nama_sub_menu'),
 		   'id_menu' => $this->post('id_menu'),
 		   'style_sub_menu' => $this->post('style_sub_menu'),
@@ -33,43 +63,61 @@ class Submenu extends REST_Controller {
 		   'grup_sub_menu' => $this->post('grup_sub_menu'),
 		   'link' => $this->post('link'),
 		);
-		$insert = $this->db->insert('sub_menu', $data);
+		$insert = $this->submenu_model->insert($data);
 		if ($insert) {
-		   $this->response($data, 200);
+		   $this->response(array('status' => 'success'), 200);
 		} else {
 		   $this->response(array('status' => 'fail', 502));
 		}
    	}
 
-   	function index_put() { // update data kontak
-		$id = $this->put('id_sub_menu');
+   	function index_put() { // update data menu
+		$id = $this->put('id');
 		$data = array(
-			'id_sub_menu' => $this->put('id_sub_menu'),
 			'nama_sub_menu' => $this->put('nama_sub_menu'),
 		   	'id_menu' => $this->put('id_menu'),
 		   	'style_sub_menu' => $this->put('style_sub_menu'),
 		   	'position_sub_menu' => $this->put('position_sub_menu'),
-		   	'grup_sub_menu' => $this->put('grup_sub_menu'),
-		   	'link' => $this->put('link'),
+		    'grup_sub_menu' => $this->put('grup_sub_menu'),
+		    'link' => $this->put('link'),
 		);
-		$this->db->where('id_sub_menu', $id);
-		$update = $this->db->update('sub_menu', $data);
+		$this->submenu_model->where('id_sub_menu = "'.$id.'"');
+		$update = $this->submenu_model->update($data);
 		if ($update) {
-			$this->response($data, 200);
+			$this->response(array('status' => 'success'), 200);
 		} else {
 			$this->response(array('status' => 'fail', 502));
 		}
  	}
 
-	function index_delete() { // delete data sub_menu
-		$id = $this->delete('id_sub_menu');
-		$this->db->where('id_sub_menu', $id);
-		$delete = $this->db->delete('sub_menu');
+	function index_delete() { // delete data menu
+		$id = $this->delete('id');
+		$this->submenu_model->where('id_sub_menu = "'.$id.'"');
+		$delete = $this->submenu_model->delete();
 		if ($delete) {
 		    $this->response(array('status' => 'success'), 201);
 		} else {
 		    $this->response(array('status' => 'fail', 502));
 		}
+	}
+
+	private function getRowDatatable($data = null) {
+		$ret = [];
+		$start = $this->get('start');
+		foreach ($data as $key => $value) {
+			$start += 1;
+			$ret[$key] = [
+				$start,
+				$value->nama_sub_menu,
+				$value->id_menu,
+				$value->style_sub_menu,
+				$value->position_sub_menu,
+				$value->grup_sub_menu,
+				$value->link,
+				'<a onclick="doFormEdit('.$value->id_sub_menu.');return false;" href="#" class="btn btn-xs btn-success">EDIT <i class="glyph-icon icon-pencil-square-o"></i></a> <a onclick="showModalBoxDelete('.$value->id_sub_menu.');return false;" href="#" class="btn btn-xs btn-danger">DELETE <i class="glyph-icon icon-close"></i></a>',
+			];
+		}
+		return $ret;
 	}
 
 }
---
+++ application/models/submenu_model.php
@@ -0,0 +1,58 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Submenu_model extends CI_Model {
+	private $_table_name = 'sub_menu';
+
+	function where($where = '') {
+		if($where != '') $this->db->where($where);
+		return $this;
+	}
+	
+	function set_limit($limit, $start = 0) {
+    	$this->db->limit($limit, $start);
+        return $this;
+    }
+	
+	function order_by($field, $direction = 'asc') {
+		$this->db->order_by($field, $direction);
+		return $this;
+	}
+	
+	function like($field, $keyword, $pattern = 'both') {
+		$this->db->like($field, $keyword, $pattern);
+		return $this;
+	}
+	
+	function or_like($field, $keyword = '', $pattern = 'both'){
+		if($keyword != '') $this->db->or_like($field, $keyword, $pattern);
+		else  $this->db->or_like($field);
+		return $this;
+	}
+	
+	function group_by($group_by = ''){
+		$this->db->group_by($group_by);
+		return $this;
+	}
+	
+	function get_row(){
+		return $this->db->get($this->_table_name)->row();
+	}
+	
+	function get_all(){
+		return $this->db->get($this->_table_name)->result();
+	}
+
+	function insert($data = null){
+		return $this->db->insert($this->_table_name, $data);
+	}
+
+	function update($data = null){
+		return $this->db->update($this->_table_name, $data);
+	}
+
+	function delete(){
+		return $this->db->delete($this->_table_name);
+	}
+}
+
+?>
